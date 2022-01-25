9ce423e0cb937f66c300bcc7c1092440ce1d61d8
himanshuupta <98265553+himanshuupta@users.noreply.github.com>
2022-01-25 21:12:16 +0530

    Day 1.2 Assignment completed (2 Files index.html and area.js)

---
+++ area.js
@@ -0,0 +1,4 @@
+const r = prompt("Enter the radius");
+document.write("Radius :" + r + "<br>");
+var res = Math.PI * r * r; // Arithmetic operator use
+document.write("Area of the circle is  :" + res);
