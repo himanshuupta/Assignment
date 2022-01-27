7bacc153353abfe97343eca1ede053e91d2307b8
himanshuupta <98265553+himanshuupta@users.noreply.github.com>
2022-01-27 21:13:26 +0530

    Assignment complete(Day 3)

---
+++ index.js
@@ -0,0 +1,9 @@
+const email = prompt("Enter e-mail address");
+const password = prompt("Enter Password");
+
+/*   -----------------if-else------------*/
+if (email == "stu@letsupgrade.com" && password == "12345") {
+    alert("You are logged in");
+} else {
+    alert("Try again with correct email and password");
+}
