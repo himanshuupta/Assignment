901addce307af1d53eb25156977f41d38a287a3b
himanshuupta <98265553+himanshuupta@users.noreply.github.com>
2022-01-27 21:20:19 +0530

    Assignment complete ternary

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const email = prompt("Enter email address");
 const password = prompt("Enter Password");
-/*---------------- Switch case ---------------- */
-switch (email && password) {
-    case "stu@letsupgrade.com" && "12345":
-        {
-            alert(" You are logged in ");
-            break;
-        }
-    default:
-        alert("Try again  with correct email and password");
-}
+/*--------------------Ternary condition ------------------*/
+email == "stu@letsupgrade.com" && password == 12345 ?
+    alert("You are logged in") :
+    alert("Try again with correct email and password");
