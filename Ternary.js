d47839f30237552cc589a2918c856f575e837667
himanshuupta <98265553+himanshuupta@users.noreply.github.com>
2022-01-28 16:10:41 +0530

    Day 3 assignment

---
+++ Ternary.js
@@ -0,0 +1,6 @@
+const email = prompt("Enter email address");
+const password = prompt("Enter Password");
+/*--------------------Ternary condition ------------------*/
+email == "stu@letsupgrade.com" && password == 12345 ?
+    alert("You are logged in") :
+    alert("Try again with correct email and password");
